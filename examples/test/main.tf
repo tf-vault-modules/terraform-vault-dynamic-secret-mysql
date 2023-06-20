99052a363b6e89eaca3c4acbbe558db4460221ea
Myros <myros.net@gmail.com>
2023-06-19 20:45:13 -0400

    Testing

--- examples/test/main.tf
+++ examples/test/main.tf
@@ -10,7 +10,7 @@ module "vault_dynamic_secret_mysql" {
   vault_mount_path = "database"
   db_username      = "vault-admin"
   db_password      = "123123"
-  db_url           = "192.168.50.241:3306"
+  db_url           = "mariadb:3306"
   connection_name  = "mysql"
   allowed_roles    = ["*"]
 
