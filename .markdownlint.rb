6163ec15f602bdcdf8c72c177ad022a48c276450
Myros <myros.net@gmail.com>
2023-06-21 13:28:50 -0400

    FIX: Updated linters

---
+++ .markdownlint.rb
@@ -0,0 +1,10 @@
+all
+
+exclude_rule 'MD002'
+exclude_rule 'MD007'
+exclude_rule 'MD029'
+exclude_rule 'MD033'
+exclude_rule 'MD041'
+
+rule 'MD013', line_length: 500
+rule 'MD026', punctuation: '.,;:!'
